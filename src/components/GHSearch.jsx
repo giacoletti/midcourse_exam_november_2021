3ca41277d00b53848f921eee4b6b2701438f7e55
giacoletti <giacoletti92@gmail.com>
2021-12-16 11:02:22 +0100

    Installs axios, redux, react-redux creates modules/network.js setting up axios with github api base url creates modules/GitHubAPI.js with searchUsers function to call api and query users adds onChange event to input in GHSearch.jsx and onClick event to button to trigger search using value from input field

--- src/components/GHSearch.jsx
+++ src/components/GHSearch.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
-import { Button, Input } from 'semantic-ui-react'
+import React, { useState } from 'react';
+import { Button, Input } from 'semantic-ui-react';
+import GitHubAPI from '../modules/GitHubAPI';
 
 const GHSearch = () => {
+  const [userSearch, setUserSearch] = useState();
+
+  const handleChange = (event) => {
+    setUserSearch(event.target.value);
+  };
+
+  const searchUsers = async () => {
+    const results = await GitHubAPI.searchUsers(userSearch);
+  }
+
   return (
     <>
-      <Input type="text" name="search" placeholder="Input GH username"/>
-      <Button name="search">Search</Button>
+      <Input
+        data-cy="gh-search-input"
+        type="text"
+        name="search"
+        onChange={handleChange}
+        placeholder="Input GH username" />
+      <Button
+        data-cy="gh-search-button"
+        onClick={searchUsers}
+        name="search">Search</Button>
     </>
-  )
-}
+  );
+};
 
-export default GHSearch
+export default GHSearch;
---
+++ src/modules/GitHubAPI.js
@@ -0,0 +1,13 @@
+import { api } from './network';
+
+const GitHubAPI = {
+  async searchUsers(query) {
+    const { data } = await api.get('search/users', {
+      accept: 'application/vnd.github.v3+json',
+      q: query
+    });
+    return data.items;
+  }
+};
+
+export default GitHubAPI;
