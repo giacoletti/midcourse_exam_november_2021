46438e14e45c0a57985537c56518ab45313a4b35
giacoletti <giacoletti92@gmail.com>
2021-12-16 12:10:23 +0100

    Fixes axios call in GitHubAPI.js

--- src/modules/GitHubAPI.js
+++ src/modules/GitHubAPI.js
@@ -3,8 +3,10 @@ import { api } from './network';
 const GitHubAPI = {
   async searchUsers(query) {
     const { data } = await api.get('search/users', {
-      accept: 'application/vnd.github.v3+json',
-      q: query
+      params: {
+        accept: 'application/vnd.github.v3+json',
+        q: query
+      }
     });
     return data.items;
   }
