ddf86ea68801046ef16175310db16c4577410cce
giacoletti <giacoletti92@gmail.com>
2021-12-16 12:01:09 +0100

    Removes unused useState from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Header from './components/Title';
 import GHSearch from './components/GHSearch';
 import { Container, Grid } from 'semantic-ui-react';
