16f3395e01b728e021b2d3b16ee7173f08806491
Giovanni Iacoletti <92155737+giacoletti@users.noreply.github.com>
2022-06-07 20:02:22 +0200

    Refactor searchUsers function in GHSearch component

--- src/components/GHSearch.jsx
+++ src/components/GHSearch.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import { Button, Input } from 'semantic-ui-react';
-import GitHubAPI from '../modules/GitHubAPI';
-import { useDispatch } from 'react-redux';
+import React, { useState } from "react";
+import { Button, Input } from "semantic-ui-react";
+import GitHubAPI from "../modules/GitHubAPI";
+import { useDispatch } from "react-redux";
 
 const GHSearch = () => {
   const [userSearch, setUserSearch] = useState();
@@ -13,15 +13,17 @@ const GHSearch = () => {
 
   const searchUsers = async () => {
     const results = await GitHubAPI.searchUsers(userSearch);
-    if (results.message){
-      dispatch({ type: 'SET_ERROR_MESSAGE', payload: results.message });
+    if (results.message) {
+      dispatch({ type: "SET_ERROR_MESSAGE", payload: results.message });
+    } else if (!results.length) {
+      dispatch({
+        type: "SET_ERROR_MESSAGE",
+        payload: "No results has been found"
+      });
     } else {
-      dispatch({ type: 'SET_SEARCH_RESULTS', payload: results });
-      if (!results.length) {
-        dispatch({ type: 'SET_ERROR_MESSAGE', payload: 'No results has been found' });
-      }
+      dispatch({ type: "SET_SEARCH_RESULTS", payload: results });
     }
-  }
+  };
 
   return (
     <>
@@ -30,11 +32,11 @@ const GHSearch = () => {
         type="text"
         name="search"
         onChange={handleChange}
-        placeholder="Input GH username" />
-      <Button
-        data-cy="gh-search-button"
-        onClick={searchUsers}
-        name="search">Search</Button>
+        placeholder="Input GH username"
+      />
+      <Button data-cy="gh-search-button" onClick={searchUsers} name="search">
+        Search
+      </Button>
     </>
   );
 };
